05187c8e1d6b79936006afd3098e66dd0e99789d
eLaubster <laubeevan@gmail.com>
2024-11-28 03:40:29 -0600

    Include multiple menus to test ability to switch between them

--- tests/panel_test.py
+++ tests/panel_test.py
@@ -12,8 +12,8 @@ def buttonFunction():
 def main(stdscr: curses.window):
     manager = UIManager(stdscr)
     mainMenu = Menu('main') 
-    manager.addMenu(mainMenu)
-    manager.switchMenu('main')
+    #manager.addMenu(mainMenu)
+    #manager.switchMenu('main')
 
     for i in range(10):
         buttonLabel = f"Button {i}"
@@ -21,10 +21,26 @@ def main(stdscr: curses.window):
         button.setLabelToResult = True
         mainMenu.addElement(str(i), button)
 
+    topLeftMenu = Menu('tl')
+    for i in range(10):
+        buttonLabel = f"TLButton {i}"
+        button = Button(buttonLabel, buttonFunction)
+        button.setLabelToResult = True
+        topLeftMenu.addElement(str(i), button)
+
+    topRightMenu = Menu('tr')
+    for i in range(10):
+        buttonLabel = f"TRButton {i}"
+        button = Button(buttonLabel, buttonFunction)
+        button.setLabelToResult = True
+        topRightMenu.addElement(str(i), button)
+
     top, bottom = manager._rootPanel.splitHorizontal()
     topLeft, topRight = top.splitVertical()
 
     bottom.loadMenu(mainMenu)
+    topLeft.loadMenu(topLeftMenu)
+    topRight.loadMenu(topRightMenu)
 
     manager.run()
 
