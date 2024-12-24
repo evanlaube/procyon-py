db194a4c997e748dc696c327dbab4650ae3e43c4
eLaubster <laubeevan@gmail.com>
2024-12-24 15:18:10 -0600

    Increment version to v0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='procyon-py',
-    version='0.3.0',
+    version='0.3.1',
     description='A terminal based UI library for Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
